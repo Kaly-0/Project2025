bfad36b94045083a1fea60202981ba67f5eff85c
Flav <flavia.fernandes@oteria.fr>
2025-02-14 00:52:56 +0100

    Ajout de motifs

---
+++ my-app-dashboard/components/SquareComponents.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SquareComponents = () => {
+    return (
+        <div>
+            <div className="square"></div>
+            <div className="square2"></div>
+            <div className="square3"></div>
+            <div className="square4"></div>
+            <div className="square5"></div>
+            <div className="square6"></div>
+            <div className="square7"></div>
+            <div className="square8"></div>
+            <div className="square9"></div>
+            <div className="square10"></div>
+        </div>
+    );
+};
+
+export default SquareComponents;
