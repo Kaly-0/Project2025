48b8654f8cef4c627ba031020a3e8afe7b1135c9
Hugo <hugo.jankowski@oteria.fr>
2025-05-12 17:04:21 +0200

    systeme authentification, page.tsx, css, modification navbar,

---
+++ my-app-dashboard/app/Register/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import React, {useState} from "react";
+import { supabase } from "@/lib/supabaseClient.ts"
+import Link from "next/link";
+import styles from "./Register.module.css";
+
+function Register() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    //const [confirmPassword, setConfirmPassword] = useState("");
+    //const [loading, setLoading] = useState(false);
+    //const [error, setError] = useState("");
+
+    const handleDiscordLogin = async () => {
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: "discord",
+        });
+
+        if (error) {
+            console.error("Erreur Discord login:", error.message);
+        }
+    };
+
+    const handleSubmit= async (event : React.FormEvent) => {
+        event.preventDefault();
+        setMessage("");
+
+        const { data, error } = await supabase.auth.signUp({
+            email : email,
+            password: password,
+        });
+
+        if (error) {
+            setMessage(error.message);
+            return;
+        }
+
+        if (data) {
+            setMessage("Votre compte a bien été créé!");
+        }
+
+        setEmail("");
+        setPassword("");
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <h2 className={styles.title}>Inscription</h2>
+                {message && <p className={styles.message}>{message}</p>}
+
+                <form onSubmit={handleSubmit} className={styles.form}>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="email"
+                        placeholder="Email"
+                        required
+                        className={styles.input}
+                    />
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        required
+                        className={styles.input}
+                    />
+                    <button type="submit" className={styles.button}>
+                        Créez votre compte
+                    </button>
+                    <button onClick={handleDiscordLogin}>
+                        Se connecter avec Discord
+                    </button>
+
+                </form>
+
+                <p className={styles.switch}>
+                    Vous avez déjà un compte?
+                    <Link href="/Login" className={styles.link}> Connectez-vous</Link>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
